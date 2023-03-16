7e40aa704a89d7351b09192271b91d9602710c5d
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-16 11:39:36 +0900

    outer join

--- join.sql
+++ join.sql
@@ -6,20 +6,38 @@
 -- 		ON e.emp_no = d.emp_no
 -- LIMIT 10;
 
--- SELECT
--- FROM 
--- 	LEFT OUTER JOIN 
--- 		ON 
+-- INSERT INTO departments
+-- VALUES ('d010', 'outer-join');
+-- 
+-- COMMIT;
+-- 
+-- SELECT*
+-- FROM departments
+
+-- SELECT d.dept_no, d.dept_name, m.emp_no
+-- FROM departments d
+-- 	LEFT OUTER JOIN dept_manager m
+-- 		ON d.dept_no = m.dept_no;
+-- WHERE 
+
+-- SELECT d.dept_no, d.dept_name, m.emp_no
+-- FROM departments d 
+-- 	RIGHT OUTER JOIN dept_manager m 
+-- 		ON d.dept_no = m.dept_no; 
 -- WHERE
 
--- SELECT
--- FROM 
--- 	RIGHT OUTER JOIN 
--- 		ON 
+-- SELECT d.dept_no, d.dept_name, m.emp_no
+-- FROM departments d  
+-- 	FULL OUTER JOIN dept_manager m  
+-- 		ON d.dept_no = m.dept_no;
 -- WHERE
-
--- SELECT
--- FROM 
--- 	FULL OUTER JOIN 
--- 		ON 
--- WHERE
+-- 마리아 db 지원 안함
+-- 적용법
+-- SELECT d.dept_no, d.dept_name, m.emp_no
+-- FROM departments d
+-- LEFT JOIN dept_manager m ON d.dept_no = m.dept_no
+-- UNION
+-- SELECT d.dept_no, d.dept_name, m.emp_no
+-- FROM departments d
+-- RIGHT JOIN dept_manager m ON d.dept_no = m.dept_no
+-- WHERE d.dept_no IS NULL;
