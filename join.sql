8ef7f3d5576f4e9fe4150a1754ba28a1ad2335a8
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-16 11:18:23 +0900

    outer join

--- join.sql
+++ join.sql
@@ -1,7 +1,25 @@
 -- join
 
-SELECT e.emp_no, e.first_name, d.dept_no
-FROM employees e
-	INNER JOIN dept_emp d
-		ON e.emp_no = d.emp_no
-LIMIT 10;
+-- SELECT e.emp_no, e.first_name, d.dept_no
+-- FROM employees e
+-- 	INNER JOIN dept_emp d
+-- 		ON e.emp_no = d.emp_no
+-- LIMIT 10;
+
+-- SELECT
+-- FROM 
+-- 	LEFT OUTER JOIN 
+-- 		ON 
+-- WHERE
+
+-- SELECT
+-- FROM 
+-- 	RIGHT OUTER JOIN 
+-- 		ON 
+-- WHERE
+
+-- SELECT
+-- FROM 
+-- 	FULL OUTER JOIN 
+-- 		ON 
+-- WHERE
